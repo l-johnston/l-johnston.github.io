bf6e32cba7e619bc729666a554d16f529496b4aa
Lee Johnston <lee.johnston.100@gmail.com>
2022-10-31 04:45:07 -0500

    Switch to Pelican and Github Actions (#1)
    * Switch to Pelican and Github Actions
    
    * Add readme
    
    Co-authored-by: Lee Johnston <lee.johnston@ni.com>

---
+++ README.md
@@ -0,0 +1,4 @@
+# Personal site
+
+The site is built using [Pelican](https://getpelican.com/) and deployed using
+[Github Actions](https://docs.github.com/en/actions).
---
+++ pelicanconf.py
@@ -0,0 +1,26 @@
+AUTHOR = "Lee Johnston"
+SITENAME = "Lee's Corner"
+SITEURL = ""
+PATH = "content"
+
+TIMEZONE = "America/Chicago"
+
+DEFAULT_LANG = "en"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
